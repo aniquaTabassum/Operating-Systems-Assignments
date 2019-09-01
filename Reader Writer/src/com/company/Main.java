b22fe4c8e17775c809ae86637eccdc9e778d1093
aniquaTabassum <aniqua.tabassum337@gmail.com>
2019-09-01 13:00:09 +0600

    Thread name in reader writer is printing

--- Reader Writer/src/com/company/Main.java
+++ Reader Writer/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.io.Reader;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 class Main {
@@ -10,9 +13,14 @@ class Main {
     static int readerCount = 0;
     static Semaphore readerSemaphore = new Semaphore(1);
     static Semaphore writerSemaphore = new Semaphore(1);
-    static Vector<Integer> vector = new Vector<Integer>(Arrays.asList(1,2,3,4));
+    static Vector<Integer> vector = new Vector<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,20,11,12));
 
     static class Read implements Runnable {
+        int id = 0;
+        public Read(int id)
+        {
+            this.id = id;
+        }
         @Override
         public void run() {
             try {
@@ -21,12 +29,13 @@ class Main {
                 if (readerCount == 1) writerSemaphore.acquire();
                 readerSemaphore.release();
 
+
                 int item = vector.get(0);
-                System.out.println("Thread "+Thread.currentThread().getName() + " is READING "+item);
+                System.out.println("Reader "+id+ " is READING "+item);
 
                 Thread.sleep(1500);
                 vector.removeElementAt(0);
-                System.out.println("Thread "+Thread.currentThread().getName() + " has FINISHED READING");
+                System.out.println("Reader "+id + " has FINISHED READING");
 
                 readerSemaphore.acquire();
                 readerCount--;
@@ -41,16 +50,21 @@ class Main {
 
     static Scanner sc = new Scanner(System.in);
     static class Write implements Runnable {
+        int id = 0;
+        public Write(int id)
+        {
+            this.id = id;
+        }
         @Override
         public void run() {
             try {
                 writerSemaphore.acquire();
                 System.out.println("Enter a number: ");
                 int n = sc.nextInt();
-                System.out.println("Thread "+Thread.currentThread().getName() + " is WRITING "+n);
+                System.out.println("Writer "+id + " is WRITING "+n);
                 Thread.sleep(2500);
                 vector.add(n);
-                System.out.println("Thread "+Thread.currentThread().getName() + " has finished WRITING");
+                System.out.println("Writer "+id + " has finished WRITING");
                 writerSemaphore.release();
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
@@ -59,21 +73,29 @@ class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        Read read = new Read();
-        Write write = new Write();
-        Thread t1 = new Thread(read);
-        t1.setName("thread1");
-        Thread t2 = new Thread(read);
-        t2.setName("thread2");
-        Thread t3 = new Thread(write);
-        t3.setName("thread3");
-        Thread t4 = new Thread(write);
-        t4.setName("thread4");
-        while(true) {
-            t1.run();
-            t3.run();
-            t2.run();
-            t4.run();
+        // Read read = new Read();
+        // Write write = new Write();
+        //Thread t1 = new Thread(read);
+        //t1.setName("thread1");
+        //Thread t2 = new Thread(read);
+        // t2.setName("thread2");
+        // Thread t3 = new Thread(write);
+        //t3.setName("thread3");
+        //Thread t4 = new Thread(write);
+        //t4.setName("thread4");
+        ExecutorService executor= Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        while (true) {
+            executor.execute(new Read(1));
+            executor.execute(new Read(2));
+            executor.execute(new Write(1));
+
+            executor.execute(new Write(2));
         }
+
+        // t1.start();
+        //t3.start();
+        //  t2.start();
+        // t4.start();
+
     }
 }
